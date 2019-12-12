f7dd92842d21f7e5cffeb503da1266ec1b457a73
jaronpoel <jaatje_1996@hotmail.com>
2019-12-12 20:36:41 +0100

    Add files via upload

---
+++ Interfaces/Contexts/IContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Interfaces.Contexts
+{
+    /// <summary>
+    /// Defines base functionality for a context.
+    /// </summary>
+    /// <typeparam name="T">Represents model correlating to context type.</typeparam>
+    public interface IContext<T>
+    {
+
+    }
+}
---
+++ Interfaces/Contexts/IUserContext.cs
@@ -0,0 +1,12 @@
+﻿using Models;
+
+namespace Interfaces.Contexts
+{
+    /// <summary>
+    /// Defines functionality for a user context.
+    /// </summary>
+    public interface IUserContext : ILogic<User>
+    {
+        void AuthenticatUser(string email, string password);
+    }
+}
