a7315ec6733cf2c6af9751aa54ed1affdb7ebe72
jaronpoel <jaatje_1996@hotmail.com>
2019-12-12 20:36:57 +0100

    Add files via upload

---
+++ Interfaces/Logic/IUserLogic.cs
@@ -0,0 +1,13 @@
+﻿using Interfaces.Logic;
+using Models;
+
+namespace Interfaces.Contexts
+{
+    /// <summary>
+    /// Defines functionality for a user context.
+    /// </summary>
+    public interface IUserLogic : ILogic<User>
+    {
+        void AuthenticatUser(string Username, string Password);
+    }
+}
